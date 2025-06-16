e0d69a994b37bf27e5214041b7a8511470bed7ac
Tomáš Slunsky <t.slunsky@gmail.com>
2025-06-16 14:14:14 +0200

    Indenting fix

--- balikobotbranches.php
+++ balikobotbranches.php
@@ -98,120 +98,120 @@ class balikobotbranches extends Module
 		/////////////////////// NAPOJENI NA DOPRAVCE ///////////////////////
 		/////////////////////// NAPOJENI NA DOPRAVCE ///////////////////////
 
-        // ČESKÁ POŠTA
-        if ($carrierCode == 'cp' && $serviceType == 'NB') {
-            if (Module::isInstalled('monster_cpost')){
-                try {
-                    $id_branch = Db::getInstance()->getValue('SELECT zip FROM '._DB_PREFIX_.'monster_cpost_expedition WHERE id_order=' . (int)$order->id);
-                } catch(Exception $e) {}
+		// ČESKÁ POŠTA
+		if ($carrierCode == 'cp' && $serviceType == 'NB') {
+			if (Module::isInstalled('monster_cpost')){
+				try {
+					$id_branch = Db::getInstance()->getValue('SELECT zip FROM '._DB_PREFIX_.'monster_cpost_expedition WHERE id_order=' . (int)$order->id);
+				} catch(Exception $e) {}
 
-                if($id_branch) {
+				if($id_branch) {
 					$package_attr_set('rec_zip', $id_branch);		//
-                }
-            } else if (Module::isInstalled('shaim_balikovna')) {
-                try {
-                    $id_branch = Db::getInstance()->getValue('SELECT psc FROM '._DB_PREFIX_.'shaim_balikovna_data JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int)$order->id);
-                } catch(Exception $e) {}
+				}
+			} else if (Module::isInstalled('shaim_balikovna')) {
+				try {
+					$id_branch = Db::getInstance()->getValue('SELECT psc FROM '._DB_PREFIX_.'shaim_balikovna_data JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int)$order->id);
+				} catch(Exception $e) {}
 
-                if($id_branch) {
+				if($id_branch) {
 					$package_attr_set('rec_zip', $id_branch);
-                }
-            } else if (Module::isInstalled('add_ceskaposta_carriers')) {
-                try {
-                    $id_branch = Db::getInstance()->getValue(
-                        'SELECT psc FROM '._DB_PREFIX_.'add_cp_all_orders o
-                        JOIN '._DB_PREFIX_.'add_cp_all_branches_B b ON o.id_branch = b.id
-                        WHERE id_order=' . (int) $order->id
-                    );
-                } catch(Exception $e) {}
+				}
+			} else if (Module::isInstalled('add_ceskaposta_carriers')) {
+				try {
+					$id_branch = Db::getInstance()->getValue(
+						'SELECT psc FROM '._DB_PREFIX_.'add_cp_all_orders o
+						JOIN '._DB_PREFIX_.'add_cp_all_branches_B b ON o.id_branch = b.id
+						WHERE id_order=' . (int) $order->id
+					);
+				} catch(Exception $e) {}
 
 				if($id_branch) {
 					$package_attr_set('rec_zip', $id_branch);
 				}
 			}
-        }
-        else if ($carrierCode == 'cp' && $serviceType == 'NP'){
-            if (Module::isInstalled('monster_cpost')) {
-                try {
-                    $id_branch = Db::getInstance()->getValue('SELECT zip FROM '._DB_PREFIX_.'monster_cpost_expedition WHERE id_order=' . (int) $order->id);
-                } catch(Exception $e) {}
-                
-                if($id_branch) {
+		}
+		else if ($carrierCode == 'cp' && $serviceType == 'NP'){
+			if (Module::isInstalled('monster_cpost')) {
+				try {
+					$id_branch = Db::getInstance()->getValue('SELECT zip FROM '._DB_PREFIX_.'monster_cpost_expedition WHERE id_order=' . (int) $order->id);
+				} catch(Exception $e) {}
+				
+				if($id_branch) {
 					$package_attr_set('rec_zip', $id_branch);
-                }
-            } else if (Module::isInstalled('add_ceskaposta_carriers')) {
-                try {
-                    $id_branch = Db::getInstance()->getValue(
-                        'SELECT psc FROM '._DB_PREFIX_.'add_cp_all_orders o
-                        JOIN '._DB_PREFIX_.'add_cp_all_branches_NP b ON o.id_branch = b.id
-                        WHERE id_order=' . (int) $order->id
-                    );
-                } catch(Exception $e) {}
+				}
+			} else if (Module::isInstalled('add_ceskaposta_carriers')) {
+				try {
+					$id_branch = Db::getInstance()->getValue(
+						'SELECT psc FROM '._DB_PREFIX_.'add_cp_all_orders o
+						JOIN '._DB_PREFIX_.'add_cp_all_branches_NP b ON o.id_branch = b.id
+						WHERE id_order=' . (int) $order->id
+					);
+				} catch(Exception $e) {}
 
 				if($id_branch) {
 					$package_attr_set('rec_zip', $id_branch);
 				}
 			} else if (Module::isInstalled('shaim_baliknapostu')) {
-                try {
-                    $id_branch = Db::getInstance()->getValue('SELECT psc FROM '._DB_PREFIX_.'shaim_baliknapostu_data JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int) $order->id);
-                } catch(Exception $e) {}
+				try {
+					$id_branch = Db::getInstance()->getValue('SELECT psc FROM '._DB_PREFIX_.'shaim_baliknapostu_data JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int) $order->id);
+				} catch(Exception $e) {}
 
-                if($id_branch) {
+				if($id_branch) {
 					$package_attr_set('rec_zip', $id_branch);
-                }
-            }
-        }
+				}
+			}
+		}
 
 
-        // DPD
-        else if ($carrierCode == 'dpd' && in_array($serviceType, [ 3 ])){
-            if(Module::isInstalled('dpdparcelshop')){
-                try {
-                    $id_branch = Db::getInstance()->getValue('SELECT zip FROM '._DB_PREFIX_.'cart_parcelshop JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int)$order->id);
-                } catch(Exception $e) {}
-                
-                if($id_branch) {
+		// DPD
+		else if ($carrierCode == 'dpd' && in_array($serviceType, [ 3 ])){
+			if(Module::isInstalled('dpdparcelshop')){
+				try {
+					$id_branch = Db::getInstance()->getValue('SELECT zip FROM '._DB_PREFIX_.'cart_parcelshop JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int)$order->id);
+				} catch(Exception $e) {}
+				
+				if($id_branch) {
 					$package_attr_set('branch_id', $country->iso_code.$id_branch);
-                }
-            }
-            if(Module::isInstalled('shaim_dpdparcelshop')){
-                try {
-                    $id_branch = Db::getInstance()->getValue('SELECT id FROM '._DB_PREFIX_.'shaim_dpdparcelshop_data JOIN '._DB_PREFIX_.'shaim_dpdparcelshop USING(id) JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int)$order->id);
-                } catch(Exception $e) {}
-                
-                if($id_branch) {
+				}
+			}
+			if(Module::isInstalled('shaim_dpdparcelshop')){
+				try {
+					$id_branch = Db::getInstance()->getValue('SELECT id FROM '._DB_PREFIX_.'shaim_dpdparcelshop_data JOIN '._DB_PREFIX_.'shaim_dpdparcelshop USING(id) JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int)$order->id);
+				} catch(Exception $e) {}
+				
+				if($id_branch) {
 					$package_attr_set('branch_id', $country->iso_code.$id_branch);
-                }
-            }
-        }
+				}
+			}
+		}
 
 
-        // GLS
-        else if ($carrierCode == 'gls' && in_array($serviceType, [ 2 ])){
-            if(Module::isInstalled('shaim_glsparcelshop')){
-                try {
-                    $id_branch = Db::getInstance()->getValue('SELECT sgd.id FROM '._DB_PREFIX_.'shaim_glsparcelshop_data sgd JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int) $order->id );
-                } catch(Exception $e) {}
+		// GLS
+		else if ($carrierCode == 'gls' && in_array($serviceType, [ 2 ])){
+			if(Module::isInstalled('shaim_glsparcelshop')){
+				try {
+					$id_branch = Db::getInstance()->getValue('SELECT sgd.id FROM '._DB_PREFIX_.'shaim_glsparcelshop_data sgd JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int) $order->id );
+				} catch(Exception $e) {}
 
-                if($id_branch) {
+				if($id_branch) {
 					$package_attr_set('branch_id', $id_branch);
-                }
-            }
-        }
+				}
+			}
+		}
 
 
-        // ULOŽENKA
-        else if ($carrierCode == 'ulozenka' && in_array($serviceType, [ 1, 11 ])){
+		// ULOŽENKA
+		else if ($carrierCode == 'ulozenka' && in_array($serviceType, [ 1, 11 ])){
 			if(Module::isInstalled('shaim_ulozenka')){
-                try {
-                    $id_branch = Db::getInstance()->getValue('SELECT id FROM '._DB_PREFIX_.'shaim_ulozenka_data JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int) $order->id);
-                    $id_branch = str_replace('ID', '', $id_branch);
-                } catch(Exception $e) {}
+				try {
+					$id_branch = Db::getInstance()->getValue('SELECT id FROM '._DB_PREFIX_.'shaim_ulozenka_data JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int) $order->id);
+					$id_branch = str_replace('ID', '', $id_branch);
+				} catch(Exception $e) {}
 			} elseif(Module::isInstalled('monster_ulozenka')){
-                try {
-                    $id_branch = Db::getInstance()->getValue('SELECT id_ulozenka FROM '._DB_PREFIX_.'monster_ulozenka_expedition WHERE id_order=' . (int) $order->id);
-                } catch(Exception $e) {}
-            } else {
+				try {
+					$id_branch = Db::getInstance()->getValue('SELECT id_ulozenka FROM '._DB_PREFIX_.'monster_ulozenka_expedition WHERE id_order=' . (int) $order->id);
+				} catch(Exception $e) {}
+			} else {
 				try { // původní oficiální modul na uloženku
 					$id_branch = Db::getInstance()->getValue('SELECT id_ulozenka FROM ' . _DB_PREFIX_ . 'ulozenka WHERE id_order=' . (int) $order->id);
 				} catch(Exception $e) {}
@@ -219,9 +219,9 @@ class balikobotbranches extends Module
 			if ($id_branch) {
 				$package_attr_set('branch_id', $id_branch);
 			}
-        }
-        else if ($carrierCode == 'ulozenka' && in_array($serviceType, [ 5 ])){
-            try {
+		}
+		else if ($carrierCode == 'ulozenka' && in_array($serviceType, [ 5 ])){
+			try {
 				$id_branch = Db::getInstance()->getValue('SELECT id_ulozenka FROM '._DB_PREFIX_.'ulozenka WHERE id_order=' . (int) $order->id);
 				if($id_branch) {
 					$package_attr_set('branch_id', $id_branch);
@@ -229,66 +229,66 @@ class balikobotbranches extends Module
 			}catch(Exception $e){}
 
 			if(!$id_branch && Module::isInstalled('dpdparcelshop')){
-                try {
-                    $id_branch = Db::getInstance()->getValue('SELECT zip FROM '._DB_PREFIX_.'cart_parcelshop JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int) $order->id);
-                }catch(Exception $e){}
-                
-                if($id_branch) {
+				try {
+					$id_branch = Db::getInstance()->getValue('SELECT zip FROM '._DB_PREFIX_.'cart_parcelshop JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int) $order->id);
+				}catch(Exception $e){}
+				
+				if($id_branch) {
 					$package_attr_set('branch_id', $country->iso_code.$id_branch);
 				}
 			}
-        }
+		}
 
 
-        // PPL
-        else if ($carrierCode == 'ppl' && in_array($serviceType, [ 46, 48 ])){
-            if(Module::isInstalled('shaim_pplparcelshop')) {
-                try {
-                    $id_branch = Db::getInstance()->getValue('SELECT id FROM '._DB_PREFIX_.'shaim_pplparcelshop_data JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int) $order->id);
-                }catch(Exception $e){}
-                
-                if($id_branch) {
+		// PPL
+		else if ($carrierCode == 'ppl' && in_array($serviceType, [ 46, 48 ])){
+			if(Module::isInstalled('shaim_pplparcelshop')) {
+				try {
+					$id_branch = Db::getInstance()->getValue('SELECT id FROM '._DB_PREFIX_.'shaim_pplparcelshop_data JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int) $order->id);
+				}catch(Exception $e){}
+				
+				if($id_branch) {
 					$package_attr_set('branch_id', $id_branch);
-                }
-            }
-        }
+				}
+			}
+		}
 
 
 		// SLOVENSKA POSTA
 		if ($carrierCode == 'sp' && $serviceType == 'BNP')
-        {
-            if(Module::isInstalled('shaim_baliknapostu')){
-                try {
-                    $id_branch = Db::getInstance()->getValue('SELECT psc FROM '._DB_PREFIX_.'shaim_baliknapostu_data JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int) $order->id);
-                }
-                catch(Exception $e) {}
+		{
+			if(Module::isInstalled('shaim_baliknapostu')){
+				try {
+					$id_branch = Db::getInstance()->getValue('SELECT psc FROM '._DB_PREFIX_.'shaim_baliknapostu_data JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int) $order->id);
+				}
+				catch(Exception $e) {}
 
-                if($id_branch) {
+				if($id_branch) {
 					$package_attr_set('rec_zip', $id_branch);
-                }
-            }
+				}
+			}
 		}
 
 
-        // ZÁSILKOVNA , 3060 - PL Packzkomat
-        else if ($carrierCode == 'zasilkovna' && in_array($serviceType, [ "VMCZ" , "VMPL", "VMSK", "VMHU", "VMRO", 3060 ])){
-            try { 
-                if(Module::isInstalled('monster_zasilkovna')) {
-                    $id_branch = Db::getInstance()->getValue('SELECT id_zasilkovna FROM '._DB_PREFIX_.'monster_zasilkovna_expedition WHERE id_order=' . (int) $order->id);
-                } elseif(Module::isInstalled('shaim_zasilkovna_widget')) {
-                    $row = Db::getInstance()->getRow('SELECT id, carrierPickupPointId FROM '._DB_PREFIX_.'shaim_zasilkovna_widget_data JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int) $order->id);
-                    //carrierPickupPointId - pobocka mimo zasilkovnu v zahranici
-                    $id_branch = $row['id'] ? $row['id'] : $row['carrierPickupPointId'];
-                } else {
-                    $id_branch = Db::getInstance()->getValue('SELECT id_branch FROM '._DB_PREFIX_.'packetery_order WHERE id_order=' . (int) $order->id);
-                }
-            }
-            catch(Exception $e) {}
+		// ZÁSILKOVNA , 3060 - PL Packzkomat
+		else if ($carrierCode == 'zasilkovna' && in_array($serviceType, [ "VMCZ" , "VMPL", "VMSK", "VMHU", "VMRO", 3060 ])){
+			try { 
+				if(Module::isInstalled('monster_zasilkovna')) {
+					$id_branch = Db::getInstance()->getValue('SELECT id_zasilkovna FROM '._DB_PREFIX_.'monster_zasilkovna_expedition WHERE id_order=' . (int) $order->id);
+				} elseif(Module::isInstalled('shaim_zasilkovna_widget')) {
+					$row = Db::getInstance()->getRow('SELECT id, carrierPickupPointId FROM '._DB_PREFIX_.'shaim_zasilkovna_widget_data JOIN '._DB_PREFIX_.'orders USING(id_cart) WHERE id_order=' . (int) $order->id);
+					//carrierPickupPointId - pobocka mimo zasilkovnu v zahranici
+					$id_branch = $row['id'] ? $row['id'] : $row['carrierPickupPointId'];
+				} else {
+					$id_branch = Db::getInstance()->getValue('SELECT id_branch FROM '._DB_PREFIX_.'packetery_order WHERE id_order=' . (int) $order->id);
+				}
+			}
+			catch(Exception $e) {}
 
-            if($id_branch) {
+			if($id_branch) {
 				$package_attr_set('branch_id', $id_branch);
-            }
-        }
+			}
+		}
 		
 	}
 
